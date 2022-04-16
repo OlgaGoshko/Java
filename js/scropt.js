d78c7ccfc699944d93dba2d6e943f1846fce84f2
OlgaGoshko <olga_goshko@ukr.net>
2022-04-16 22:47:48 +0300

    first java

---
+++ js/scropt.js
@@ -0,0 +1,21 @@
+let name = prompt("Введи своє ім'я")
+alert("Привіт, " + name + "!")
+
+
+let age = prompt("Введіть свій рік народження")
+const year = 2022
+alert("Вам " + (year - age) + "!")
+
+
+let length = prompt("Введіть довжину сторони квадрата")
+alert("P=" + length*4)
+
+
+let radius = prompt("Введіть радіус кола")
+const NumberPI = 3.14
+alert("S=" + NumberPI*(radius)**2)
+
+
+let distance = prompt("Введіть відстань у кілометрах між містом А і містом В")
+let hour = prompt("За скільки годин ви хочете дістатись до міста В")
+alert("Ваша швидкість повинна бути - " + distance/hour + " км/год")
